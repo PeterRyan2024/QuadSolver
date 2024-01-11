b2d30f9bbf77890d9b1b1d239b3016948e2ab4d9
Peter Ryan <pryan24@students.polytechnic.org>
2024-01-11 09:21:04 -0800

    Added a story

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,5 +1,13 @@
 public class QuadraticSolver{
     public static void main(String[] args) {
         System.out.println("Welcome to the Quadratic Solver!");
+        System.out.println("Wait, how did you get in here...");
+        System.out.println(".");
+        System.out.println("..");
+        System.out.println("...");
+        System.out.println("I'm calling the cops!!!");
+        System.out.println("WAIT");
+        System.out.println("What's that you're holding?");
+        System.out.println("IS THAT A....");
     }
 }
