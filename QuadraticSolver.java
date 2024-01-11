9f255c9b477d3bdae34cab6a1b565a837d273031
Peter Ryan <pryan24@students.polytechnic.org>
2024-01-11 09:23:00 -0800

    Added more distress to the reader

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -2,9 +2,12 @@ public class QuadraticSolver{
     public static void main(String[] args) {
         System.out.println("Welcome to the Quadratic Solver!");
         System.out.println("Wait, how did you get in here...");
+        System.out.println("Both my doors are locked");
+        System.out.println("WHO ARE YOU!?!?");
         System.out.println(".");
         System.out.println("..");
         System.out.println("...");
+        System.out.println("I can't take this today");
         System.out.println("I'm calling the cops!!!");
         System.out.println("WAIT");
         System.out.println("What's that you're holding?");
